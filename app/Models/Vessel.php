4f4cc15d6dafd30df1aea4bb1a5ea16bb8e1d38d
Yasel Heredia Rojas <kkimerag@gmail.com>
2023-12-08 11:32:41 -0500

    Some Models Methods

--- app/Models/Vessel.php
+++ app/Models/Vessel.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Vessel extends Model
 {
     use HasFactory;
+
+    public function type()
+    {
+        return $this->belongsTo(Type::class);
+    }
+
+    public function location()
+    {
+        return $this->belongsTo(Location::class);
+    }
+
+    public function rate()
+    {
+        return $this->belongsTo(Rate::class);
+    }
 }
