cf2b4bf0019e090e27f126d8fd24a76f8158b7ee
Yasel Heredia Rojas <kkimerag@gmail.com>
2023-12-08 05:02:34 -0500

    api route

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('payments')->group(function () {
+    Route::get('/create-connect-stripe', [StripeController::class, 'connectToStripe']);
+    Route::get('/check-onboarding', [StripeController::class, 'checkOnboarding']);
+    Route::get('/get-stripe-link', [StripeController::class, 'CreateAccountLink']);
+    Route::get('/stripe-account-exist', [StripeController::class, 'stripeAccountExist']);
+    Route::get('/get-publishable-key', [StripeController::class, 'GetPublishableKey']);
+    Route::post('/create-stripe-payment-intent', [StripeController::class, 'stripePaymentIntent']);
+    Route::post('/create-checkout-session', [StripeController::class, 'createCheckoutSession']);
+    Route::post('/verify-check-out', [StripeController::class, 'verifyCheckoutSession'])->name('subscription.verify');
+});
