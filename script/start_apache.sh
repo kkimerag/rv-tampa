565febfed446a5ced06b141dfbb21bd8a25d7443
Yasel Heredia Rojas <kkimerag@gmail.com>
2024-01-15 11:56:22 -0500

    deployment scrips

---
+++ script/start_apache.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Stop Apache
+sudo service apache2 start
+
+# Other custom actions if needed
---
+++ script/stop_apache.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Stop Apache
+sudo service apache2 stop
+
+# Other custom actions if needed
