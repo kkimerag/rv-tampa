5657db14657dfeeeb18400b640c87c3e85feb3da
Yasel Heredia Rojas <kkimerag@gmail.com>
2024-01-17 00:11:18 -0500

    modifying the VM creation code

--- scripts/stop_apache.sh
+++ scripts/stop_apache.sh
@@ -9,45 +9,45 @@ logDirectory="/var/log/httpd/$domain"
 virtualHostFile="/etc/httpd/conf.d/$domain.conf"
 
 # Check if the virtual host already exists
-if [ -e "$virtualHostFile" ]; then
-    echo "Virtual host for $domain already exists. Exiting."
-    exit 1
-fi
+if [ ! -e "$virtualHostFile" ]; then
+    # Create document root and log directory
+    sudo mkdir -p $documentRoot
+    sudo mkdir -p $logDirectory
 
-# Create document root and log directory
-sudo mkdir -p $documentRoot
-sudo mkdir -p $logDirectory
+    # Create a virtual host configuration file
+    sudo tee $virtualHostFile > /dev/null <<EOL
+    <VirtualHost *:80>
+        ServerAdmin admin@ciruxdigital.com
+        ServerName $domain
+        ServerAlias www.$domain
+        DocumentRoot $documentRoot
 
-# Create a virtual host configuration file
-sudo tee $virtualHostFile > /dev/null <<EOL
-<VirtualHost *:80>
-    ServerAdmin admin@ciruxdigital.com
-    ServerName $domain
-    ServerAlias www.$domain
-    DocumentRoot $documentRoot
-
-    <Directory "$documentRoot">
-        Options Indexes FollowSymLinks
-        AllowOverride All
-        Require all granted
-    </Directory>
-
-    ErrorLog ${APACHE_LOG_DIR}/error.log
-    CustomLog ${APACHE_LOG_DIR}/access.log combined
-
-    <IfModule mod_rewrite.c>
-        <Directory "/var/www/html/public">
-            RewriteEngine On
-            RewriteBase /
-            RewriteCond %{REQUEST_FILENAME} !-f
-            RewriteCond %{REQUEST_FILENAME} !-d
-            RewriteRule ^(.*)$ index.php/$1 [L]
+        <Directory "$documentRoot">
+            Options Indexes FollowSymLinks
+            AllowOverride All
+            Require all granted
         </Directory>
-    </IfModule>
-</VirtualHost>
+
+        ErrorLog ${APACHE_LOG_DIR}/error.log
+        CustomLog ${APACHE_LOG_DIR}/access.log combined
+
+        <IfModule mod_rewrite.c>
+            <Directory "/var/www/html/public">
+                RewriteEngine On
+                RewriteBase /
+                RewriteCond %{REQUEST_FILENAME} !-f
+                RewriteCond %{REQUEST_FILENAME} !-d
+                RewriteRule ^(.*)$ index.php/$1 [L]
+            </Directory>
+        </IfModule>
+    </VirtualHost>
 EOL
 
-echo "Virtual host configuration for $domain is complete."
+    echo "Virtual host configuration for $domain is complete."
+else
+    echo "Virtual host for $domain already exists. Skipping creation."
+fi
+
 
 # Stop Apache
 sudo systemctl stop httpd
