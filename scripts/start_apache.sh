3f2c0cd9883f16edc823f11f710f2f22a4d1b541
Yasel Heredia Rojas <kkimerag@gmail.com>
2024-01-16 23:35:30 -0500

    test before install

--- scripts/start_apache.sh
+++ scripts/start_apache.sh
@@ -2,3 +2,10 @@
 
 # Stop Apache
 sudo systemctl start httpd
+
+# Optimization
+sudo php /var/www/html/rv-tampa.com/artisan optimize
+
+# Set Laravel Permissions
+sudo chown -R apache:apache /var/www/html/rv-tampa.com
+sudo chmod -R 755 /var/www/html/rv-tampa.com/storage
